6554fd4ff944ecad3b874f52e6dfa96a4a02d1a7
James Lewis <jamesblewislewis@gmail.com>
2017-07-22 18:39:56 +1000

    added key findings page

--- MMOS/js/main.js
+++ MMOS/js/main.js
@@ -7,16 +7,17 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 } else {
   $( "#gif" ).replaceWith( '<video poster="img/hd1080.png" id="bgvid" class="fade video" playsinline autoplay loop muted><source src="Anti-piracy-opt.mp4" type="video/mp4"></video>' );
   function setPlaySpeed() {
-      vid.playbackRate = 0.5;
+  vid.playbackRate = 0.5;
   }
+  var myvid = $('#bgvid')[0];
+  $(window).scroll(function(){
+    var scroll = $(this).scrollTop();
+    scroll > 70 ? myvid.pause() : myvid.play()
+  })
 }
 
 $(document).ready(function(){
-    var myvid = $('#bgvid')[0];
-    $(window).scroll(function(){
-      var scroll = $(this).scrollTop();
-      scroll > 70 ? myvid.pause() : myvid.play()
-    })
+
     // very inificient make better
 		$('#scope').on('click', function(event) {
     if ( $( ".scope" ).is( ":hidden" ) ) {
