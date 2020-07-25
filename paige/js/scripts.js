39ecc22cb556006fae56848ca94b9ef6945c59de
James Lewis <jamesblewislewis@gmail.com>
2020-07-25 20:28:29 +1000

    added fact 3

--- paige/js/scripts.js
+++ paige/js/scripts.js
@@ -1,16 +1,17 @@
 const mainText = document.getElementsByClassName("magic-text");
 const fact = " Say... wanna hear a random fact? ";
 const factsHandler = () => {
-    const randomNumber = Math.floor(Math.random() * Math.floor(3));
+    const randomNumber = Math.floor(Math.random() * Math.floor(4));
     switch (randomNumber) {
         case 0:
-            return "\nJeepers what a significant birthday you have! Lot’s of crazy things have happened on June 7th over the years. Henry II is crowned king of Germany. The Americans are victorious at the battle of midway. Most importantly also the same birthday as Iggy Azalea, coincidence?🤔 I think not! 🥳"
+            return "\nJeepers what a significant birthday you have! Lot’s of crazy things have happened on June 7th over the years. Henry II was crowned king of Germany. The Americans were victorious at the battle of midway. Most importantly also the same birthday as Iggy Azalea, coincidence?🤔 I think not! 🥳"
         case 1: 
             return "\nWell did you know that over 30 words rhyme with Paige🤩? I'm gonna tell you some of them. age, cage, gage, gauge, mage, page, rage, sage, stage, swage, wage,assuage, backstage, encage, engage, enrage, incage, offstage, onstage, Osage, restage, uncage, upstage, disengage and... one more for good luck, preengage. 🥱 gosh that was a lot of words! Really makes you appreciate the need to have a website with your name on it to list them all🤣";
         case 2:
-            return `\nHoly crap have you ever looked up your name in the dictionary? 😂 I have, I’m not kidding this is what it says: Paige
-            A girl who is pretty, but doesnt think it. She sometimes lacks in the confidence department and has some rough days. She's funny and smiles a lot. She won't tolerate being called "hot" or "sexy" by a boy. A guy who says that doesn't have a chance. She has loads of friends and has her sassy moments. She loves music and can't dance, but does anyway. Paige has been through a lot and hates when people are negative towards her. She's amazing, but doesnt know it. Get to know her and she'll be less awkward.`;
-    }
+            return "\nHoly crap have you ever looked up your name in the dictionary? 😂 I have, I’m not kidding this is what it says: Paige A girl who is pretty, but doesnt think it. She sometimes lacks in the confidence department and has some rough days. She's funny and smiles a lot. She won't tolerate being called "hot" or "sexy" by a boy. A guy who says that doesn't have a chance. She has loads of friends and has her sassy moments. She loves music and can't dance, but does anyway. Paige has been through a lot and hates when people are negative towards her. She's amazing, but doesnt know it. Get to know her and she'll be less awkward.`;
+        case 3:
+            return "\nPaige you are such a bubbly awesome person. This isn’t even a ‘random fact’ I just love and apricate you and just how easy you are to talk too. You are such a chill lovely friend, I just wanted to share some love🥰."
+        }
 }
 
 var currentSentenceEnd = 0;
