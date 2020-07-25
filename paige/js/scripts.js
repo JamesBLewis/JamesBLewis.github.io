b042b9f7d8b3964a4813c01e5d90d73d68bb512f
James Lewis <jamesblewislewis@gmail.com>
2020-07-25 21:49:31 +1000

    fixed double fun fact call

--- paige/js/scripts.js
+++ paige/js/scripts.js
@@ -6,7 +6,7 @@ const factsHandler = () => {
         case 0:
             return `\nJeepers what a significant birthday you have! Lot’s of crazy things have happened on June 7th over the years. Henry II was crowned king of Germany. The Americans were victorious at the battle of midway. Most importantly also the same birthday as Iggy Azalea, coincidence?🤔 I think not! 🥳`;
         case 1: 
-            return `\nWell did you know that over 30 words rhyme with Paige🤩? I'm gonna tell you some of them. age, cage, gage, gauge, mage, page, rage, sage, stage, swage, wage,assuage, backstage, encage, engage, enrage, incage, offstage, onstage, Osage, restage, uncage, upstage, disengage and... one more for good luck, preengage. 🥱 gosh that was a lot of words! Really makes you appreciate the need to have a website with your name on it to list them all🤣`;
+            return `\nWell did you know that over 30 words rhyme with Paige🤩? I'm gonna tell you some of them. age, cage, gage, gauge, mage, page, rage, sage, stage, swage, wage,assuage, backstage, encage, engage, enrage, incage, offstage, onstage, Osage, restage, uncage, upstage, disengage and... one more for good luck, preengage. 🥱 gosh, that was a lot of words! Really makes you appreciate the need to have a website with your name on it to list them all🤣`;
         case 2:
             return `\nHoly crap have you ever looked up your name in the dictionary? 😂 I have, I’m not kidding this is what it says: Paige A girl who is pretty, but doesnt think it. She sometimes lacks in the confidence department and has some rough days. She's funny and smiles a lot. She won't tolerate being called 'hot' or 'sexy' by a boy. A guy who says that doesn't have a chance. She has loads of friends and has her sassy moments. She loves music and can't dance, but does anyway. Paige has been through a lot and hates when people are negative towards her. She's amazing, but doesnt know it. Get to know her and she'll be less awkward.`;
         case 3:
@@ -31,7 +31,6 @@ const startPrint = ( target, text, speed) => {
 
 const DisplaycrazyText  = (text, speed, target, factsHandler) => {
     target.addEventListener("input", function() {
-        console.log(currentSentenceEnd);
         if (["ok", "plz", "ye", "more"].some(function(substring) { 
             return target.innerHTML
             .substring(currentSentenceEnd - 1)
@@ -39,6 +38,7 @@ const DisplaycrazyText  = (text, speed, target, factsHandler) => {
             // she said yes :)
             displayedText = `. Ha! I was hoping you'd say yes 😅.${factsHandler()}`;
             startPrint(target, displayedText, 70);
+            currentSentenceEnd = target.innerHTML.length
         }
     }, false);
     startPrint(target, text, 60);
